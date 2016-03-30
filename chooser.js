0125745eac5238b25e4629a92292517a94de24ec
Samuel Lijin <sxlijin+github@gmail.com>
2016-03-30 02:40:32 -0500

    mvp done

---
+++ chooser.js
@@ -0,0 +1,64 @@
+// CSS IDs of <input> elems to listen for input on
+var inputElemIDs = ["colorPalette", "colorValue", "courseName"];
+
+
+var clearInput = function(e) { e.target.value = ""; };
+
+var getInputElementById = 
+    function(id) { return document.querySelector(`input#${id}`); };
+
+var getValueOfInputID = 
+    function(id) { return getInputElementById(id).value; };
+
+var setValueOfInputID = 
+    function(id, val) { getInputElementById(id).value = val; };
+
+var setBackgroundColorOfInputs = 
+    function(color) {
+        for (var i = 0; i < inputElemIDs.length; i++) {
+            getInputElementById(inputElemIDs[i]).style.backgroundColor = color;
+        }
+    };
+
+var setCourseToColor =
+    function(course, color) {
+        chrome.tabs.query(
+            {active: true, currentWindow: true}, 
+            function(tabs) {
+                console.log(tabs);
+                chrome.tabs.sendMessage(tabs[0].id, 
+                    {"courseName":course, "color":color}, 
+                    function(response) { console.log(response); }
+                );
+            }
+        );
+    };
+
+var parseInput = 
+    function(e) {
+        var color = e.target.value;
+        if (e.target.id === "colorPalette") {
+            setValueOfInputID("colorValue", color);
+        }
+        else if (e.target.id === "colorValue") {
+            setValueOfInputID("colorPalette", color);
+        }
+        else {
+            color = getValueOfInputID("colorPalette");
+        }
+            
+        var course = getValueOfInputID("courseName");
+        //console.log(color);
+        setBackgroundColorOfInputs(color);
+        setCourseToColor(course, color);
+    };
+
+var onDOMLoad = 
+    function() {
+        for (var i = 0; i < inputElemIDs.length; i++) {
+            var inputElem = document.getElementById(inputElemIDs[i]);
+            inputElem.addEventListener("input", parseInput);
+        }
+    }
+
+document.addEventListener('DOMContentLoaded', onDOMLoad);
