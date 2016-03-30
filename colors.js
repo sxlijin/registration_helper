34a3f5b41f0300d40a2e40c20f94ab919e9944e1
Samuel Lijin <sxlijin+github@gmail.com>
2016-03-30 02:39:47 -0500

    colors.js cleaned up and event listener added

--- colors.js
+++ colors.js
@@ -1,23 +1,41 @@
-var getArrayOf = 
-    function(s) { 
-        return [].slice.call(document.getElementsByClassName('eventSaved')).
-            filter(function(elem) { 
-                return elem.tagName === "DIV" && elem.innerHTML === s 
-                }) 
-    }
+var selector = '.eventSaved'; // CSS selector corresp. to courses
 
-var setColor = 
+var getDivsForCSS = 
+    function(s) {
+        return document.querySelectorAll('div' + s);
+    };
+
+var getArrayOfCourses = 
+    function(course_name) {
+        return [...new Set(getDivsForCSS(selector).map())]
+    };
+
+var getArrayForCourse = 
+    function(s) { 
+        var matchingCourse = function(elem) { return elem.innerHTML === s; };
+        return [].slice.call(getDivsForCSS(selector)).filter(matchingCourse);
+    };
+
+var setCourseToColor = 
     function(s, color) {
-        elems = getArrayOf(s);
+        var elems = getArrayForCourse(s);
         for (var i = 0; i < elems.length; i++) {
             elems[i].style.backgroundColor = color;
         }
-    }
+    };
 
-var resetColor = function(s) { setColor(s, '#4f8edc') }
+var resetColor = function(s) { setCourseToColor(s, '#4f8edc') };
 
-var removeFocus = function(s) { setColor(s, '#6faaff') }
+var removeFocus = function(s) { setCourseToColor(s, '#6faaff') };
 
 var restoreFocus = resetColor
 
-var strongFocus = function(s) { setColor(s, "#4f8e8c") }
+var strongFocus = function(s) { setCourseToColor(s, "#4f8e8c") };
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) { 
+        //console.log('received request:');
+        //console.log(request);
+        setCourseToColor(request.courseName, request.color); 
+    }
+);
