982f0cbcf493d24f785f1d0c6f43108affd743e0
Samuel Lijin <sxlijin+github@gmail.com>
2016-03-30 22:16:54 -0500

    persistent storage and colors.js implemented

--- chooser.js
+++ chooser.js
@@ -1,64 +1,55 @@
 // CSS IDs of <input> elems to listen for input on
 var inputElemIDs = ["colorPalette", "colorValue", "courseName"];
 
-var clearInput = function(e) { e.target.value = ""; };
+function getInputElementById(id) 
+    { return document.querySelector(`input#${id}`); }
 
-var getInputElementById = 
-    function(id) { return document.querySelector(`input#${id}`); };
+function getValueOfInputID(id) { return getInputElementById(id).value; }
 
-var getValueOfInputID = 
-    function(id) { return getInputElementById(id).value; };
+function setValueOfInputID(id, val) { getInputElementById(id).value = val; }
 
-var setValueOfInputID = 
-    function(id, val) { getInputElementById(id).value = val; };
-
-var setColorOfInputs = 
-    function(color) {
-        getInputElementById(inputElemIDs[0]).style.value = color;
-        for (var i = 1; i < inputElemIDs.length; i++) {
-            getInputElementById(inputElemIDs[i]).style.backgroundColor = color;
-        }
-    };
-
-var setCourseToColor =
-    function(course, color) {
-        chrome.tabs.query(
-            {active: true, currentWindow: true}, 
-            function(tabs) {
-                //console.log(tabs);
-                chrome.tabs.sendMessage(tabs[0].id, 
-                    {"courseName":course, "color":color}
-                    //function(response) { console.log(response); }
-                );
-            }
-        );
-    };
-
-var parseInput = 
-    function(e) {
-        var color = e.target.value;
-        if (e.target.id === "colorPalette") {
-            setValueOfInputID("colorValue", color);
-        }
-        else if (e.target.id === "colorValue") {
-            setValueOfInputID("colorPalette", color);
-        }
-        else {
-            color = getValueOfInputID("colorPalette");
-        }
-            
-        var course = getValueOfInputID("courseName");
-        //console.log(color);
-        setColorOfInputs(color);
-        setCourseToColor(course, color);
-    };
-
-var onDOMLoad = 
-    function() {
-        for (var i = 0; i < inputElemIDs.length; i++) {
-            var inputElem = document.getElementById(inputElemIDs[i]);
-            inputElem.addEventListener("input", parseInput);
-        }
+function setColorOfInputs(color) {
+    getInputElementById(inputElemIDs[0]).style.value = color;
+    for (var i = 1; i < inputElemIDs.length; i++) {
+        getInputElementById(inputElemIDs[i]).style.backgroundColor = color;
     }
+}
+
+function setCourseToColor(course, color) {
+    chrome.tabs.query(
+        {active: true, currentWindow: true}, 
+        function(tabs) {
+            chrome.tabs.sendMessage(
+                tabs[0].id, 
+                {"courseName":course, "color":color}
+            );
+        }
+    );
+}
+
+function parseInput(e) {
+    var color = e.target.value;
+    if (e.target.id === "colorPalette") {
+        setValueOfInputID("colorValue", color);
+    }
+    else if (e.target.id === "colorValue") {
+        setValueOfInputID("colorPalette", color);
+    }
+    else {
+        color = getValueOfInputID("colorPalette");
+    }
+        
+    var course = getValueOfInputID("courseName");
+    //console.log(color);
+    setColorOfInputs(color);
+    setCourseToColor(course, color);
+};
+
+function onDOMLoad() {
+    for (var i = 0; i < inputElemIDs.length; i++) {
+        var inputElem = document.getElementById(inputElemIDs[i]);
+        inputElem.addEventListener("input", parseInput);
+    }
+}
 
 document.addEventListener('DOMContentLoaded', onDOMLoad);
