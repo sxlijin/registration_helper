d3b4f9cd05117d7d749e343cf0e62f599e96187a
Samuel Lijin <sxlijin+github@gmail.com>
2016-03-31 03:02:34 -0500

    background.js needed for comms

---
+++ background.js
@@ -0,0 +1,15 @@
+function broadcastMessage(message) {
+    message.origin = "background.js";
+    chrome.tabs.query(
+        {active: true, currentWindow: true}, 
+        function(tabs) { chrome.tabs.sendMessage(tabs[0].id, message); }
+    );
+}
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(request);
+        if (!request.origin) return;
+        if (request.origin === "xhrEventHandler.js") broadcastMessage(request);
+    }
+);
